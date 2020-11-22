1bbf16725a104324db57c132a71ced3973e04153
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-22 17:18:38 +0530

    React App.js

---
+++ src/js/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+	const title = "hello world";
+	const enhancedTitle = title + " -React App";
+	return <div>{enhancedTitle}</div>;
+}
+
+export default App;
