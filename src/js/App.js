039e778139b33a3b68029b85ffb3d0fba9119fb5
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-22 18:26:13 +0530

    use function defined in preload.js to send notification

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-
 function App() {
 	const title = "hello world";
-	const enhancedTitle = title + " -React App";
-	return <div>{enhancedTitle}</div>;
+    const enhancedTitle = title + " -React App";
+
+
+    function sendNotifiation() {
+			e_notification.sendNotification("This is my custom message");
+		}
+
+		return (
+			<div>
+				{enhancedTitle}
+				<button onClick={sendNotifiation}>Send Notification</button>
+			</div>
+		);
 }
 
 export default App;
