502b8a1b8cfd3e91cfa969489421ca098f6714dd
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-27 13:49:29 +0530

    Fetch chat action

---
+++ src/js/actions/chats.js
@@ -0,0 +1,12 @@
+import * as api from "../api/chats";
+export function fetchChats() {
+	return async function (dispatch) {
+		const chats = await api.fetchChats();
+		dispatch({
+			type: "CHATS_FETCH_SUCCESS",
+			chats,
+		});
+		console.log(chats);
+		return chats;
+	};
+}
