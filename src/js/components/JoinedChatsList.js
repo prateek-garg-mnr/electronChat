fef7ca3f8660c8c75cb59bc3a978209ca2a458bf
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-27 13:46:32 +0530

    joined chats screen

---
+++ src/js/components/JoinedChatsList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ChatSearch from "./ChatSearch";
+import { useHistory } from "react-router-dom";
+function JoinedChats({ chats }) {
+	const history = useHistory();
+
+	return (
+		<div className="list-container">
+			<ChatSearch />
+			<ul className="items">
+				{chats.map((chat) => {
+					return (
+						<li
+							key={chat.id}
+							onClick={() => history.push(`/chat/${chat.id}`)}
+							className="item"
+						>
+							<div className="item-status">
+								<img src={chat.image} alt="Retail Admin" />
+								<span className="status online"></span>
+							</div>
+							<p className="name-time">
+								<span className="name mr-2">{chat.name}</span>
+							</p>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+
+export default JoinedChats;
