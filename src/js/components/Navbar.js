7ebf6417a55200946237b56e215c646f1f937c46
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-27 13:47:12 +0530

    Navbar component

---
+++ src/js/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+function Navbar() {
+	const history = useHistory();
+
+	return (
+		<div className="chat-navbar">
+			<nav className="chat-navbar-inner">
+				<div className="chat-navbar-inner-left">
+					<button
+						onClick={() => history.goBack()}
+						className="btn btn-outline-primary"
+					>
+						Back
+					</button>
+					<Link to="/settings" className="btn btn-outline-success ml-2">
+						Settings
+					</Link>
+				</div>
+				<div className="chat-navbar-inner-right">
+					<span className="logged-in-user">Hi User</span>
+
+					<Link to="/" className="btn btn-outline-success ml-2">
+						Login
+					</Link>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default Navbar;
