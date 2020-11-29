e0c1db7858d9936ff6c609ceb2fc09a9cbe2c86c
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-29 20:21:53 +0530

    Add reducer to redux store

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import reduxThunk from "redux-thunk";
 import chatReducer from "../reducers/chats";
+import authReducer from "../reducers/auth";
 export default function configureStore() {
 	const store = createStore(
 		combineReducers({
 			chats: chatReducer,
+			auth: authReducer,
 		}),
 		applyMiddleware(reduxThunk)
 	);
