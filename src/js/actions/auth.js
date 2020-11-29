d0545beeb9ea222a8c1560311f173e1bd32460cf
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-29 20:21:11 +0530

    Add action to check the change in auth state

--- src/js/actions/auth.js
+++ src/js/actions/auth.js
@@ -1,13 +1,23 @@
 import * as api from "../api/auth";
 
-export const registerUser = (formData) => {
-	return async function (dispatch) {
-		const user = await api.register(formData);
-		console.log(user);
-		dispatch({
-			type: "AUTH_REGISTER_SUCCESS",
-			user,
-		});
-		return user;
-	};
+export const registerUser = (formData) => async (dispatch) => {
+	const user = await api.register(formData);
+	dispatch({
+		type: "AUTH_REGISTER_SUCCESS",
+		user,
+	});
 };
+
+export const listenToAuthChanges = () => (dispatch) => {
+	dispatch({ type: "AUTH_ON_INIT" });
+	api.onAuthStateChange((authUser) => {
+		if (authUser) {
+			dispatch({ type: "AUTH_ON_SUCCESS", user: authUser });
+			console.log("we are authenticated ");
+		} else {
+			dispatch({ type: "AUTH_ON_ERROR", user: authUser });
+			console.log("we are not authenticated");
+		}
+	});
+};
+
