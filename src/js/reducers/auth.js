56c9eb152ec603c23f81cdc5337280405c91a29c
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-30 17:18:40 +0530

    Catch action types for auth initialization in auth reducers

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -5,12 +5,19 @@ const DEFAULT_STATE = {
 
 export default function authReducer(state = DEFAULT_STATE, action) {
 	switch (action.type) {
+		case "AUTH_REGISTER_INIT":
+		case "AUTH_LOGIN_INIT":
+			return { ...state, isChecking: true };
+		case "AUTH_REGISTER_SUCCESS":
+		case "AUTH_LOGIN_SUCCESS":
+			return { ...state, isChecking: false };
 		case "AUTH_ON_INIT":
 			return {
 				user: null,
 				isChecking: true,
 			};
 		case "AUTH_ON_SUCCESS":
+			console.log('action.............',action);
 			return {
 				user: action.user,
 				isChecking: false,
