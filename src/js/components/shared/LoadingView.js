e7e9208d921074543a09f45e6ed9107005a423ea
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-11-30 17:18:03 +0530

    Add loader jsx

---
+++ src/js/components/shared/LoadingView.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Loader from "./Loader";
+function LoadingView({ message = "Just one moment please!!!" }) {
+	return (
+		<div className="loading-screen">
+			<div className="loading-view">
+				<div className="loading-view-container">
+					<div className="mb-3">{message}</div>
+					<Loader />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default LoadingView;
