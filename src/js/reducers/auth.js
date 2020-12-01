4d2fc86c51389346ace693a4b1f9b78be66960df
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-12-01 19:03:49 +0530

    Separate login and reducers and catch actions on error and update loading state

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -1,33 +1,63 @@
-const DEFAULT_STATE = {
-	user: null,
-	isChecking: false,
-};
+import { combineReducers } from "redux";
 
-export default function authReducer(state = DEFAULT_STATE, action) {
+function loginReducer(state = { error: null }, action) {
 	switch (action.type) {
-		case "AUTH_REGISTER_INIT":
 		case "AUTH_LOGIN_INIT":
-			return { ...state, isChecking: true };
-		case "AUTH_REGISTER_SUCCESS":
-		case "AUTH_LOGIN_SUCCESS":
-			return { ...state, isChecking: false };
-		case "AUTH_ON_INIT":
-			return {
-				user: null,
-				isChecking: true,
-			};
-		case "AUTH_ON_SUCCESS":
-			console.log('action.............',action);
-			return {
-				user: action.user,
-				isChecking: false,
-			};
-		case "AUTH_ON_ERROR":
-			return {
-				user: null,
-				isChecking: false,
-			};
+			return { error: null };
+		case "AUTH_LOGIN_ERROR":
+			return { error: action.error };
 		default:
 			return state;
 	}
 }
+
+function registerReducer(state = { error: null }, action) {
+	switch (action.type) {
+		case "AUTH_REGISTER_INIT":
+			return { error: null };
+		case "AUTH_REGISTER_ERROR":
+			return { error: action.error };
+		default:
+			return state;
+	}
+}
+
+function createAuthReducers() {
+	const user = (state = null, action) => {
+		switch (action.type) {
+			case "AUTH_ON_INIT":
+				return null;
+			case "AUTH_ON_SUCCESS":
+				return action.user;
+			case "AUTH_ON_ERROR":
+				return null;
+			default:
+				return state;
+		}
+	};
+
+	const isChecking = (state = false, action) => {
+		switch (action.type) {
+			case "AUTH_REGISTER_INIT":
+			case "AUTH_LOGIN_INIT":
+			case "AUTH_ON_INIT":
+				return true;
+			case "AUTH_ON_SUCCESS":
+			case "AUTH_ON_ERROR":
+			case "AUTH_LOGIN_ERROR":
+			case "AUTH_REGISTER_ERROR":
+				return false;
+			default:
+				return state;
+		}
+	};
+
+	return combineReducers({
+		user,
+		isChecking,
+		login: loginReducer,
+		register: registerReducer,
+	});
+}
+
+export default createAuthReducers();
