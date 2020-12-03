f710a72323c553ce9464a725ceed36fe4f16fbc9
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-12-04 03:14:37 +0530

    dispaction action to Subscribe to chat and pass it as prop to chatuserlist

--- src/js/views/Chat.js
+++ src/js/views/Chat.js
@@ -1,24 +1,39 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import ChatUsersList from "../components/ChatUsersList";
-import ChatMessagesList from "../components/ChatMessagesList";
-import ViewTitle from "../components/shared/ViewTitle";
-import BaseLayout from "../layouts/Base"
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import ChatUsersList from '../components/ChatUsersList';
+import ChatMessagesList from '../components/ChatMessagesList';
+import ViewTitle from '../components/shared/ViewTitle';
+import BaseLayout from '../layouts/Base';
+import { subscribeToChat } from '../actions/chats';
+
 function Chat() {
-	const { id } = useParams();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+	const activeChat = useSelector(({ chats }) => {
+		return chats.activeChats[id]
+	})
+
+  useEffect(() => {
+    const unsubFromChat = dispatch(subscribeToChat(id));
+    return () => {
+      unsubFromChat();
+    }
+  }, [])
+	
 	return (
 		<BaseLayout>
-		<div className="row no-gutters fh">
-			<div className="col-3 fh">
-				<ChatUsersList />
-			</div>
-			<div className="col-9 fh">
-				<ViewTitle text={`Joined Channel: ${id}`} />
-				<ChatMessagesList />
-			</div>
+			<div className="row no-gutters fh">
+				<div className="col-3 fh">
+					<ChatUsersList users={ activeChat ? activeChat.joinedUsers : [] } />
+					
+				</div>
+				<div className="col-9 fh">
+					<ViewTitle text={`Joined Channel: ${activeChat ? activeChat.name : '' }`} />
+					<ChatMessagesList />
+				</div>
 			</div>
 		</BaseLayout>
-			
 	);
 }
 
