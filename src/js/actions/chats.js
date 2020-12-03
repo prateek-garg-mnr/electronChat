c59250264266492b8e2726b05a35dbcb0cc2cb77
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-12-04 00:43:37 +0530

    separate joined and available chats and add action to join chat

--- src/js/actions/chats.js
+++ src/js/actions/chats.js
@@ -2,24 +2,36 @@ import * as api from "../api/chats";
 import db from "../db/firestore";
 export const fetchChats = () => async (dispatch, getState) => {
 	const { user } = getState().auth;
-	dispatch({type: 'CHATS_FETCH_INIT'});
+	dispatch({ type: "CHATS_FETCH_INIT" });
 	const chats = await api.fetchChats();
-  
-	chats
-	  .forEach(chat => chat.joinedUsers = chat.joinedUsers.map(user => user.id));
-  
-	const sortedChats = chats.reduce((accuChats, chat) => {
-	  accuChats[chat.joinedUsers.includes(user.uid) ? 'joined' : 'available'].push(chat);
-	  return accuChats;
-	}, {joined: [], available: []})
-  
+
+	chats.forEach(
+		(chat) => (chat.joinedUsers = chat.joinedUsers.map((user) => user.id))
+	);
+
+	const sortedChats = chats.reduce(
+		(accuChats, chat) => {
+			accuChats[
+				chat.joinedUsers.includes(user.uid) ? "joined" : "available"
+			].push(chat);
+			return accuChats;
+		},
+		{ joined: [], available: [] }
+	);
+
 	dispatch({
-	  type: 'CHATS_FETCH_SUCCESS',
-	  ...sortedChats
-	})
-  
+		type: "CHATS_FETCH_SUCCESS",
+		...sortedChats,
+	});
+	console.log(sortedChats);
 	return sortedChats;
-  }
+};
+
+export const joinChat = (chat, uid) => (dispatch) => {
+	api.joinChat(uid, chat.id).then((_) => {
+		dispatch({ type: "CHATS_JOIN_SUCCESS" });
+	});
+};
 
 export const createChat = (formData, userId) => async (dispatch) => {
 	try {
