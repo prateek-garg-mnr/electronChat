8c148e242ef9b707729f314a2e668d6fd01fd2e0
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-12-04 00:45:29 +0530

    create a function to return reducer

--- src/js/reducers/chats.js
+++ src/js/reducers/chats.js
@@ -1,11 +1,34 @@
-const DEFAULT_STATE = {
-	items: [],
-};
-export default function chatReducer(state = DEFAULT_STATE, action) {
-	switch (action.type) {
-		case "CHATS_FETCH_SUCCESS":
-			return { items: action.chats };
-		default:
-			return state;
-	}
+import { combineReducers } from "redux";
+
+function createChatReducer() {
+	const joined = (state = [], action) => {
+		switch (action.type) {
+			case "CHAT_FETCH_RESTART":
+				return [];
+			case "CHATS_FETCH_SUCCESS":
+				return action.joined;
+			default: {
+				return state;
+			}
+		}
+	};
+
+	const available = (state = [], action) => {
+		switch (action.type) {
+			case "CHAT_FETCH_RESTART":
+				return [];
+			case "CHATS_FETCH_SUCCESS":
+				return action.available;
+			default: {
+				return state;
+			}
+		}
+	};
+
+	return combineReducers({
+		joined,
+		available,
+	});
 }
+
+export default createChatReducer();
