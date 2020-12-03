4d3b4622544b77e257dd2360c5af40c66161abed
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2020-12-03 23:13:52 +0530

    Action to create chat

--- src/js/actions/chats.js
+++ src/js/actions/chats.js
@@ -1,4 +1,5 @@
 import * as api from "../api/chats";
+import db from "../db/firestore";
 export function fetchChats() {
 	return async function (dispatch) {
 		const chats = await api.fetchChats();
@@ -10,3 +11,21 @@ export function fetchChats() {
 		return chats;
 	};
 }
+
+export const createChat = (formData, userId) => async (dispatch) => {
+	try {
+		let newChat = { ...formData };
+		// reference to the user
+		const useRef = db.doc(`profiles/${userId}`);
+
+		// chat admin
+		newChat.admin = useRef;
+		// create chat
+		const chatId = await api.createChat(newChat);
+		dispatch({ type: "CHAT_CREEATE_SUCCESS" });
+		// join chat
+		await api.joinChat(userId, chatId);
+		dispatch({ type: "CHATS_JOIN_SUCCESS" });
+		return chatId;
+	} catch (e) {}
+};
